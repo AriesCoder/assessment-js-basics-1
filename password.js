2f8ca09e065e02860216de2a91251642c6fbfbdc
Trang Lam <ms.tranglam@gmail.com>
2022-01-21 11:19:13 -0800

    adding ASCII Art

--- password.js
+++ password.js
@@ -12,24 +12,59 @@ reader.question("Please enter a password to validate: ", function(input){
 
     if(password.length >= 10){
         if(password.indexOf(' ') >= 0){
-            console.log("Failure! Please type a password without the white space(s)")
+            console.log(`                                     
+___________      .__.__                        
+\_   _____/____  |__|  |  __ _________   ____  
+ |    __) \__  \ |  |  | |  |  \_  __ \_/ __ \ 
+ |     \   / __ \|  |  |_|  |  /|  | \/\  ___/ 
+ \___  /  (____  /__|____/____/ |__|    \___  >
+     \/        \/                           \/  `);
+            console.log("Please type a password without the white space(s)")
             console.log(`Your password is ${password}`)
         }else{
-            console.log("Success!")
+            console.log(`
+       _____                               
+  / ___/__  _______________  __________
+  \__ \/ / / / ___/ ___/ _ \/ ___/ ___/
+ ___/ / /_/ / /__/ /__/  __(__  |__  ) 
+/____/\__,_/\___/\___/\___/____/____/  
+                                                                                                                                    
+            `)
             console.log(`Your password is ${password}`)
         }
     }
     else if(password.length > 0){
         if (password.indexOf(' ') >= 0){
-            console.log("Failure! Please type a password with at least 10 characters and no the white space(s)")
+            console.log(`                                     
+___________      .__.__                        
+\_   _____/____  |__|  |  __ _________   ____  
+ |    __) \__  \ |  |  | |  |  \_  __ \_/ __ \ 
+ |     \   / __ \|  |  |_|  |  /|  | \/\  ___/ 
+ \___  /  (____  /__|____/____/ |__|    \___  >
+     \/        \/                           \/  `);
+            console.log("Please type a password with at least 10 characters and no the white space(s)")
             console.log(`Your password is ${password}`)
         }else{
-            console.log('Failure! The password should have at least 10 characters')
+            console.log(`                                     
+___________      .__.__                        
+\_   _____/____  |__|  |  __ _________   ____  
+ |    __) \__  \ |  |  | |  |  \_  __ \_/ __ \ 
+ |     \   / __ \|  |  |_|  |  /|  | \/\  ___/ 
+ \___  /  (____  /__|____/____/ |__|    \___  >
+     \/        \/                           \/  `);
+            console.log('The password should have at least 10 characters')
             console.log(`Your password is ${password}`)
         }
     }
     else{
-        console.log("Failure! Please type a password to validate")
+        console.log(`                                     
+___________      .__.__                        
+\_   _____/____  |__|  |  __ _________   ____  
+ |    __) \__  \ |  |  | |  |  \_  __ \_/ __ \ 
+ |     \   / __ \|  |  |_|  |  /|  | \/\  ___/ 
+ \___  /  (____  /__|____/____/ |__|    \___  >
+     \/        \/                           \/  `);
+        console.log("Please type a password to validate")
     }
 
     reader.close()
